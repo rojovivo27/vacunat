c3056b9fa36b3c684b47654e4690b36951253cc0
rojovivo27 <rojovivo_27@hotmail.com>
2025-06-18 10:34:56 -0600

    Fixing when there are no users saved

--- Vacuna-T/Vacuna-T/Presentation/Profile/ProfileView.swift
+++ Vacuna-T/Vacuna-T/Presentation/Profile/ProfileView.swift
@@ -17,45 +17,55 @@ struct ProfileView: View {
     var body: some View {
         VStack {
             Spacer()
-            let years = Calendar.current.dateComponents([.year], from: vm.selectedUser?.dateOfBirth ?? Date.now, to: Date.now).year ?? 0
-            let imageName = (vm.selectedUser?.gender ?? 0) == 0 ? years < 3 ? "006-baby" : years < 15 ? "004-boy" : years < 60 ? "007-man" : "002-old-man" : years < 3 ? "005-baby-1" : years < 15 ? "003-girl" : years < 60 ? "008-girl-1" : "001-old-woman"
-            Image(imageName)
-                .resizable()
-                .clipShape(.rect(cornerRadius: 8))
-                .frame(width: 80, height: 80)
-                .padding()
-            
-            Picker("Select User", selection: $vm.selectedUser) {
-                ForEach(vm.users) { user in
-                    Text(user.name).tag(user.self)
+            if vm.selectedUser == nil {
+                Text("Primero debe crear un perfil. Agregue un nuevo perfil presionando el botón de 'Agregar nuevo perfil'.")
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+            } else {
+                VStack {
+                    let years = Calendar.current.dateComponents([.year], from: vm.selectedUser?.dateOfBirth ?? Date.now, to: Date.now).year ?? 0
+                    let imageName = (vm.selectedUser?.gender ?? 0) == 0 ? years < 3 ? "006-baby" : years < 15 ? "004-boy" : years < 60 ? "007-man" : "002-old-man" : years < 3 ? "005-baby-1" : years < 15 ? "003-girl" : years < 60 ? "008-girl-1" : "001-old-woman"
+                    Image(imageName)
+                        .resizable()
+                        .clipShape(.rect(cornerRadius: 8))
+                        .frame(width: 80, height: 80)
+                        .padding()
+                    
+                    Picker("Select User", selection: $vm.selectedUser) {
+                        ForEach(vm.users) { user in
+                            Text(user.name).tag(user.self)
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                    
+                    Button {
+                        isEdition = true
+                    } label: {
+                        Text("Editar perfil")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Button(role: .destructive) {
+                        if let id = vm.selectedUser?.id {
+                            vm.deleteUser(id: id)
+                            vm.saveUsers()
+                            vm.selectedUser = vm.users.first
+                        }
+                    } label: {
+                        Text("Eliminar perfil")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .padding(.bottom, 32)
+                    
+                    if vm.selectedUser != nil {
+                        Text(vm.selectedUser?.dateOfBirth.formatted(date: .abbreviated, time: .omitted) ?? "")
+                        if let dob = vm.selectedUser?.dateOfBirth {
+                            let components = Calendar.current.dateComponents([.year, .month, .day], from: dob, to: Date.now)
+                            Text("\(components.year ?? 0) años, \(components.month ?? 0) meses y \(components.day ?? 0) días")
+                        }
+                    }
                 }
             }
-            .pickerStyle(.wheel)
-            
-            Button {
-                isEdition = true
-            } label: {
-                Text("Editar perfil")
-            }
-            .buttonStyle(.borderedProminent)
-            
-            Button(role: .destructive) {
-                if let id = vm.selectedUser?.id {
-                    vm.deleteUser(id: id)
-                    vm.saveUsers()
-                    vm.selectedUser = vm.users.first
-                }
-            } label: {
-                Text("Eliminar perfil")
-            }
-            .buttonStyle(.borderedProminent)
-            .padding(.bottom, 32)
-            
-            if vm.selectedUser != nil {
-                Text(vm.selectedUser?.dateOfBirth.formatted(date: .abbreviated, time: .omitted) ?? "")
-                Text("\(Calendar.current.dateComponents([.year], from: vm.selectedUser?.dateOfBirth ?? Date.now, to: Date.now).year ?? 0) años")
-            }
-            
             Spacer()
             
             Button {
